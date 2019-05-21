225795ec0d10a7e6a5d07eee39e0b0590a666cb7
Nicccccc <34077481+Nicccccc@users.noreply.github.com>
2019-05-21 14:16:20 -0700

    Add files via upload
    fixed issues I created when merging amani's previous pull request to master

--- GodotTest/Scenes/mainGame.gd
+++ GodotTest/Scenes/mainGame.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-
+#attack still broken
 const Utils = preload("res://Scripts/Utils.gd")
 
 export var runSpeed : float = 220
@@ -23,20 +23,6 @@ export var airMvmtTime := {
 	decel = 0.2,
 	turn = 0.5
 }
-#export var accelTimeG := 0.2
-#export var decelTimeG := 0.1
-#export var turnTimeG := 0.1
-#export var accelTimeA := 0.2
-#export var decelTimeA := 0.4
-#export var turnTimeA := 0.1
-
-
-#export var cancels := {
-#	"Idle": false,
-#	"Fall": false,
-#	"Attack": false,
-#	"Jump": false
-#}
 
 #Child nodes
 onready var animP : AnimationPlayer = $AnimationPlayer
@@ -51,19 +37,51 @@ func _physics_process(delta):
 	
 	var gravity = 2*jumpHeight/(jumpTime*jumpTime)
 	velocity.y += gravity*delta
-	
 
+	var onFloor = is_on_floor()
+	var onWall = is_on_wall()
+
+	#movement
+	if(Input.is_action_pressed("ui_right")):
+		move += 1
+		#sprite.flip_h = velocity.x < 0
+		sprite.flip_h = false
+		if onFloor:
+			playAnim("playerRun")
+	elif Input.is_action_pressed("ui_left"):
+		move -= 1
+		#sprite.flip_h = velocity.x < 0
+		sprite.flip_h = true
+		if onFloor:
+			playAnim("playerRun")
+	else:
+		move = 0
+		if canIdle:
+			playAnim("playerIdle")
+
+	if Input.is_action_just_pressed("jump"):
+		#Ground jump
+		if onFloor:
+			velocity.y = -2*jumpHeight/jumpTime
+			playAnim("playerJump")
+		#Wall jump
+		elif onWall:
+			canFall = false
+			playAnim("playerFlip")
+			velocity.y = -2*jumpHeight/jumpTime
+
+	#falling animation
+	if !onFloor and velocity.y > 0 and canFall:
+		playAnim("playerFalling")
+
+	#attack animations
+	if Input.is_action_just_pressed("light attack") and canFall:
+		playAnim("playerLightAttack")
+	
 		#animation runs on a different frame system than the script
-		#I set the canFall value in animation and it kept getting cancelled 
-		#do state checks in script, not animation, as they are faster here (constant vs variable update)
-		#canFall = false
-		#canIdle = false
-		playAnim("playerLightAttack", false, false)
-		print("Can idle is now ", canIdle)
-	
-	
+
 	#movement calculations
-	var moveTimes = groundMvmtTime if onFloor else airMvmtTime
+	var moveTimes = groundMvmtTime if is_on_floor() else airMvmtTime
 	
 	var accelX : float
 	#Calculate acceleration
@@ -82,11 +100,6 @@ func _physics_process(delta):
 	
 	# -y is up, +y is down
 	velocity = move_and_slide(velocity, Vector2(0, -1))
-
-
-#func _on_AnimationPlayer_animation_finished(anim_name):
-#	canIdle = true
-#	canFall = true
 
 #play animation and reset vars
 func playAnim(animName: String, fall:=true, idle:=true):
